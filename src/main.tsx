2795252cf800ef31e13ceecc6738267861402b0f
Daniel Seres <seres.jozsef.dani@gmail.com>
2024-03-24 15:25:50 +0100

    add theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { PrimeReactProvider } from 'primereact/api';
+
+import './styles.css';
+import './assets/themes/material/material-light/compact/deeppurple/theme.scss'
+
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>,
 )
